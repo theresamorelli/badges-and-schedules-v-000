3d028eb811126ec102e3bb1058b24a28566b28f7
Theresa Morelli <theresa.l.morelli@gmail.com>
2018-05-15 01:01:04 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+   array.map { |name| badge_maker(name) }
+end
+
+def assign_rooms(array)
+  array.map do |name|
+    "Hello #{name}! You'll be assigned to room #{index}!"
+  end
+
+end
+
+def conference_badges()
+
+end
