0fa81e98a67d4baa25f4208d8f0b503e2ebf887c
Theresa Morelli <theresa.l.morelli@gmail.com>
2018-05-15 01:15:30 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,6 +17,9 @@ def printer(name_array)
   batch_badge_creator(name_array).each do |badge|
     puts badge
   end
+  assign_rooms(name_array).each do |message|
+    puts message
+  end  
 end
 
 def conference_badges()
