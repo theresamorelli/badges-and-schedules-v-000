0e8013aa25fe71c20c52da1177f7f376033c18fa
Theresa Morelli <theresa.l.morelli@gmail.com>
2018-05-15 01:12:36 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,18 +3,18 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(array)
-   array.map { |name| badge_maker(name) }
+def batch_badge_creator(name_array)
+   name_array.map { |name| badge_maker(name) }
 end
 
-def assign_rooms(array)
-  array.each_with_index.map do |name, index|
+def assign_rooms(name_array)
+  name_array.each_with_index.map do |name, index|
     "Hello, #{name}! You'll be assigned to room #{index+1}!"
   end
 end
 
-def printer
-
+def printer(name_array)
+  batch_badge_creator(name_array)
 end
 
 def conference_badges()
